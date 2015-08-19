83785a8daa6eecaf11617603a63f7fefbdb915c2
Dan Bender <hello@danbender.net>
2015-08-19 07:01:00 +0200

    manual non-storyboard setup of collection view: create collectionView instance; set up dataSource and Delegate to conform to the protocols UICollectionViewDataSource, UICollectionViewDelegate; implement two required collectionView functions

--- ExchangeAGram/FilterViewController.swift
+++ ExchangeAGram/FilterViewController.swift
@@ -8,14 +8,29 @@
 
 import UIKit
 
-class FilterViewController: UIViewController {
+class FilterViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
     
     var thisFeedItem: FeedItem!
-
+    
+//    write collectionView in code, not drag'n drop it in storyboard
+    var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+//    write collectionView in code, not drag'n drop it in storyboard
+        let layout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        
+//        item size (cell size)
+        layout.itemSize = CGSize(width: 150.0, height: 150.0)
+        
+//         actually create collectionView
+        collectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,15 +38,11 @@ class FilterViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
     }
-    */
-
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
 }
