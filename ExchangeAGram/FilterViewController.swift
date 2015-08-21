c88379c556b592508fc3a5eca144c66b90ac8757
Dan Bender <hello@danbender.net>
2015-08-21 06:31:12 +0200

    manipulate properties of filter by setting setValue() func

--- ExchangeAGram/FilterViewController.swift
+++ ExchangeAGram/FilterViewController.swift
@@ -7,6 +7,8 @@ class FilterViewController: UIViewController, UICollectionViewDataSource, UIColl
 //    write collectionView in code, not drag'n drop it in storyboard
     var collectionView: UICollectionView!
     
+    let kIntensity = 0.7
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -65,6 +67,16 @@ class FilterViewController: UIViewController, UICollectionViewDataSource, UIColl
         let unsharpen = CIFilter(name: "CIUnsharpMask")
         let monochrome = CIFilter(name: "CIColorMonochrome")
         
+//        adjust filters by setting the setValue() func
+        let colorControls = CIFilter(name: "CIColorControls")
+        colorControls.setValue(0.5, forKey: kCIInputSaturationKey)
+        
+        let sepia = CIFilter(name: "CISepiaTone")
+        sepia.setValue(kIntensity, forKey: kCIInputIntensityKey)
+        
+        let colorClamp = CIFilter(name: "CIColorClamp")
+        colorClamp.setValue(CIVector(x: 0.9, y: 0.9, z: 0.9, w: 0.9), forKey: "inputMaxComponents")
+        colorClamp.setValue(CIVector(x: 0.2, y: 0.2, z: 0.2, w: 0.2), forKey: "inputMinComponents")
         
         return []
     }
