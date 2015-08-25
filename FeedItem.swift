b7d35f44d2e1a8a4a07aaf908442284b2368c18b
Dan Bender <hello@danbender.net>
2015-08-25 06:59:48 +0200

    added ThumbNail to FeedItem; grouped it and ExchangeAGram.xcdatamodel in Model group

--- FeedItem.swift
+++ FeedItem.swift
@@ -1,11 +1,3 @@
-//
-//  FeedItem.swift
-//  ExchangeAGram
-//
-//  Created by Dan Bender on 17/08/15.
-//  Copyright (c) 2015 Dan Bender. All rights reserved.
-//
-
 import Foundation
 import CoreData
 
@@ -14,5 +6,6 @@ class FeedItem: NSManagedObject {
 
     @NSManaged var caption: String
     @NSManaged var image: NSData
+    @NSManaged var thumbNail: NSData
 
 }
