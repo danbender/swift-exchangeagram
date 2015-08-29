776443234349663e85023e97cede0411585dbdd6
Dan Bender <hello@danbender.net>
2015-08-29 23:57:53 +0200

    cacheImage()

--- ExchangeAGram/FilterViewController.swift
+++ ExchangeAGram/FilterViewController.swift
@@ -9,6 +9,7 @@ class FilterViewController: UIViewController, UICollectionViewDataSource, UIColl
     var context:CIContext = CIContext(options: nil)
     var filters:[CIFilter] = []
     let placeHolderImage = UIImage(named: "Placeholder")
+    let tmp = NSTemporaryDirectory()
     
     
     override func viewDidLoad() {
@@ -137,6 +138,19 @@ class FilterViewController: UIViewController, UICollectionViewDataSource, UIColl
         
     }
     
+//    caching functions
+    func cacheImage(imageNumber: Int) {
+        let fileName = "\(imageNumber)"
+        let uniquePath = tmp.stringByAppendingPathComponent(fileName)
+        
+        if !NSFileManager.defaultManager().fileExistsAtPath(fileName) {
+            let data   = self.thisFeedItem.thumbNail
+            let filter = self.filters[imageNumber]
+            let image  = filteredImageFromImage(data, filter: filter)
+            
+            UIImageJPEGRepresentation(image, 1.0).writeToFile(uniquePath, atomically: true)
+        }
+    }
     
     
     
