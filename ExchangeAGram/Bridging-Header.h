6e50db1c88eb1d7beb8cfb90432f3b6baaeaab20
Dan Bender <hello@danbender.net>
2015-09-05 17:43:58 +0800

    add bridge-header and add it to build settings

---
+++ ExchangeAGram/Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  Bridging-Header.h
+//  ExchangeAGram
+//
+//  Created by Dan Bender on 05/09/15.
+//  Copyright (c) 2015 Dan Bender. All rights reserved.
+//
+
+#import <FacebookSDK/FacebookSDK.h>
