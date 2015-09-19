0b750c1e125caddb85b1743b84530ffd65544292
Dan Bender <hello@danbender.net>
2015-09-19 12:12:15 +0200

    MapViewController

--- ExchangeAGram/MapViewController.swift
+++ ExchangeAGram/MapViewController.swift
@@ -1,11 +1,3 @@
-//
-//  MapViewController.swift
-//  ExchangeAGram
-//
-//  Created by Dan Bender on 19/09/15.
-//  Copyright (c) 2015 Dan Bender. All rights reserved.
-//
-
 import UIKit
 
 class MapViewController: UIViewController {
@@ -21,15 +13,4 @@ class MapViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
