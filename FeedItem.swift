1006b0b0b83a863a53c97d18f99d8e9f1a37a606
Dan Bender <hello@danbender.net>
2015-09-19 13:10:20 +0200

    edit data modal to include lat and long (both: double), add ObjC bridge

--- FeedItem.swift
+++ FeedItem.swift
@@ -1,11 +1,22 @@
+//
+//  FeedItem.swift
+//  ExchangeAGram
+//
+//  Created by Dan Bender on 19/09/15.
+//  Copyright (c) 2015 Dan Bender. All rights reserved.
+//
+
 import Foundation
 import CoreData
 
 @objc (FeedItem)
+
 class FeedItem: NSManagedObject {
 
     @NSManaged var caption: String
     @NSManaged var image: NSData
     @NSManaged var thumbNail: NSData
+    @NSManaged var latitude: NSNumber
+    @NSManaged var longitude: NSNumber
 
 }
