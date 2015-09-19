7932d40ccc96b5a906053d16dc28d4779f12e927
Dan Bender <hello@danbender.net>
2015-09-19 14:08:08 +0200

    fetch request from coredata to return image and location data; display all pins on a map; but for now zoom in on the last one

--- ExchangeAGram/MapViewController.swift
+++ ExchangeAGram/MapViewController.swift
@@ -1,5 +1,6 @@
 import UIKit
 import MapKit
+import CoreData
 
 class MapViewController: UIViewController {
 
@@ -10,18 +11,29 @@ class MapViewController: UIViewController {
 
         // Do any additional setup after loading the view.
         
-        let location = CLLocationCoordinate2DMake(48.868639224587, 2.37119161036255)
-//        to determine the amount of area spanned by the map.
-        let span = MKCoordinateSpanMake(0.05, 0.05)
-//        to determine what the center should be and how much area should be shown
-        let region = MKCoordinateRegionMake(location, span)
-        mapView.setRegion(region, animated: true)
+        let request = NSFetchRequest(entityName: "FeedItem")
+        let appDelegate:AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        let context:NSManagedObjectContext = appDelegate.managedObjectContext!
+        var error:NSError?
+        let itemArray = context.executeFetchRequest(request, error: &error)
+        println(error)
         
-        let annotation = MKPointAnnotation()
-        annotation.coordinate = location
-        annotation.title = "Canal Saint-Martin"
-        annotation.subtitle = "Paris"
-        mapView.addAnnotation(annotation)
+        if itemArray!.count > 0 {
+            for item in itemArray! {
+                let location = CLLocationCoordinate2D(latitude: Double(item.latitude), longitude: Double(item.longitude))
+                let span = MKCoordinateSpanMake(0.05, 0.05)
+                let region = MKCoordinateRegion(center: location, span: span)
+                
+//                TO DO: since this is set in a for loop it's now going to have the last item that was added.
+                mapView.setRegion(region, animated: true)
+                
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = location
+                annotation.title = item.caption
+    
+                mapView.addAnnotation(annotation)
+            }
+        }
         
     }
 
